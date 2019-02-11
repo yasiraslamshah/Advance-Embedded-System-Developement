5ad19e65980086bb34a65491738d60d5bb32ab99
Yasir Aslam Shah <31644060+yasiraslamshah@users.noreply.github.com>
2019-02-10 22:45:25 -0700

    Homework 2
    Homework 2
    AESD

---
+++ sortingyas.h
@@ -0,0 +1,9 @@
+//author: yasir aslam shah
+//HW2
+//AESD 2019
+#ifndef __YASIR_H
+#define __YASIR_H
+
+void sortingyas(int32_t  __user* buffer1,int32_t __user* buffer2, int32_t sizeofbuffer);
+
+#endif
